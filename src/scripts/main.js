b1975befe1c060004a80322074cdec3dd66c928c
Jonnynho <ricenice1221@gmail.com>
2025-09-10 10:44:16 -0300

    adicionando ps modulos de header, footer e FAQ no site e melhorando com transições usando JavaScript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){ //variaveis e DOMContentLoaded
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]')
+    const heroSection = document.querySelector('.hero');
+    const heroHeigth = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+
+        if(posicaoAtual < heroHeigth){
+            ocultaHeader();
+        }else{
+            exibeHeader();
+        }
+    })
 
     for (let i = 0; i < buttons.length; i++){ //mostra a aba
         buttons[i].addEventListener('click', function(button){
@@ -8,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){ //variaveis e DOMConte
             const tab = document.querySelector(`[data-tab-id=${tabTarget}]`)
 
 
-            hideTab();
+            escondeAba();
             tab.classList.add('shows__list--is-active');
             removeBotaoAtivo()
             button.target.classList.add('shows__tabs__button--is-active');
@@ -16,9 +29,31 @@ document.addEventListener('DOMContentLoaded', function(){ //variaveis e DOMConte
             
         })
     }
+
+    for (let i = 0; i < questions.length; i++){  //mostra e esconde a aba de perguntas
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
 
-function removeBotaoAtivo(){
+function ocultaHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+
+function abreOuFechaResposta(elemento){
+    const openedQuestion = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(openedQuestion);
+}
+
+function removeBotaoAtivo(){ //remove o border do botão ativo e passa para o ativo
     const buttons = document.querySelectorAll('[data-tab-button]');
 
     for (let i = 0; i < buttons.length; i++){
@@ -27,7 +62,7 @@ function removeBotaoAtivo(){
             }
 }
 
-function hideTab(){ //esconde a aba
+function escondeAba(){ //esconde a aba
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for(let i = 0; i< tabsContainer.length; i++)
